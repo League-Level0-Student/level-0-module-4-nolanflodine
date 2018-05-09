08540df247bc63ff1d44da0c8362d85511576877
Flodine.Nolan <Flodine.Nolan@SEMS-259-35.SEMS259.LAB>
2018-05-09 14:05:10 -0700

    Chooooooooooooooose_your_own_adventure

--- src/Choooooose_your_own_adventure.java
+++ src/Choooooose_your_own_adventure.java
@@ -3,7 +3,34 @@ import javax.swing.JOptionPane;
 public class Choooooose_your_own_adventure {
 	public static void main(String[] args) {
 JOptionPane.showMessageDialog(null, "Once there was an ugly barnacle.  It was so ugly that everyone died.  The end.");
-int a= JOptionPane.showMessageDialog(null, "Did you like the story?", "",
-JOptionPane.YES_NO_CANCEL_OPTION);
+int a=  JOptionPane.showConfirmDialog(null, "Did you like the story?", "",
+        JOptionPane.YES_NO_OPTION);
+if (a==0) {
+JOptionPane.showMessageDialog(null, "You are ligitness.");
+int c=  JOptionPane.showConfirmDialog(null, "Do you like trains?", "",
+        JOptionPane.YES_NO_OPTION);
+if (c==0) {
+JOptionPane.showMessageDialog(null, "You have gained my trust.");
+JOptionPane.showMessageDialog(null, "You are a good person.");
+}
+else {
+JOptionPane.showMessageDialog(null, "You have lost my trust.");
+JOptionPane.showMessageDialog(null, "You are a bad person.");	
+}
+}
+else {
+JOptionPane.showMessageDialog(null, "You suck because you are barnaclist!");
+int b=  JOptionPane.showConfirmDialog(null, "Do you think you can wright a better story?", "",
+        JOptionPane.YES_NO_OPTION);
+if (b==0) {
+JOptionPane.showInputDialog(null, "Then I challenge you to wright a better one.");
+JOptionPane.showMessageDialog(null, "Your story sucks.");
+JOptionPane.showMessageDialog(null, "Next time don't judge a barnacle by its cover.");
+}
+else {
+JOptionPane.showMessageDialog(null, "That's what I thought.");
+JOptionPane.showMessageDialog(null, "Next time don't judge a barnacle by its cover.");
+}
+}
 	}
 }
